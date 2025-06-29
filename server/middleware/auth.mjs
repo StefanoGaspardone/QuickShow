9965973d9985f202d56947d638a382231593931f
Stefano Gaspardone <s337201@studenti.polito.it>
2025-06-29 22:30:18 +0200

    status code added

--- server/middleware/auth.mjs
+++ server/middleware/auth.mjs
@@ -7,20 +7,26 @@ export const protectAdmin = async (req, res, next) => {
         const user = await clerkClient.users.getUser(userId);
 
         if(user.privateMetadata.role !== 'admin') {
-            return res.json({ success: false, message: 'Not Authorized' });
+            return res.status(403).json({ success: false, message: 'Not Authorized' });
         }
 
         next();
     } catch(error) {
         console.log(error);
-        return res.json({ success: false, message: error.message });
+        return res.status(500).json({ success: false, message: error.message });
     }
 }
 
 export const requireAuth = async (req, res, next) => {
+    try {
     const { userId } = req.auth();
 
     if(!userId) return res.status(401).json({ success: false, message: 'Unauthorized' });
 
     next();
+
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: error.message });
+    }
 };
